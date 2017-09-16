7c18db49ddc6e0fa67e14fa716e6a6fed5790266
sayands1997 <sayandsarkar.1997@gmail.com>
2017-09-17 00:18:31 +0530

    Initial Client Commit

--- services/passport.js
+++ services/passport.js
@@ -24,18 +24,16 @@ passport.use(
       callbackURL: "/auth/google/callback",
       proxy: true
     },
-    (accessToken, refreshToken, profile, done) => {
-      User.findOne({ googleId: profile.id }).then(existingUser => {
-        if (existingUser) {
-          //we already have a record with the given profile ID
-          done(null, existingUser);
-        } else {
-          //we dont have an user with this id
-          new User({ googleId: profile.id })
-            .save()
-            .then(user => done(null, user));
-        }
-      });
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleId: profile.id });
+
+      if (existingUser) {
+        //we already have a record with the given profile ID
+        return done(null, existingUser);
+      }
+      //we dont have an user with this id
+      const user = await new User({ googleId: profile.id }).save();
+      done(null, user);
     }
   )
 );
