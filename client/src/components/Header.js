33220e5a78f3e364f65048cbdb73ca4a06d8dd94
sayands <sayandsarkar.1997@gmail.com>
2017-10-21 21:43:05 +0530

    Setup Payment Handler

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
+import Payments from "./Payments";
 
 class Header extends Component {
   renderContent() {
@@ -16,11 +17,14 @@ class Header extends Component {
         );
 
       default:
-        return (
-          <li>
+        return [
+          <li key="1">
+            <Payments />
+          </li>,
+          <li key="2">
             <a href="/api/logout">Logout</a>
           </li>
-        );
+        ];
     }
   }
   render() {
---
+++ client/src/components/Payments.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+class Payments extends Component {
+  render() {
+    return (
+      <StripeCheckout
+        name="Emaily"
+        description="$5 for 5 survey credits"
+        amount={500}
+        token={token => console.log(token)}
+        stripeKey={process.env.REACT_APP_STRIPE_KEY}
+      >
+        <button className="btn">Add Credits</button>
+      </StripeCheckout>
+    );
+  }
+}
+
+export default Payments;
