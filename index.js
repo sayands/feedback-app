cd61c1fa11ccf3d8c5e686a612c16733fa09032e
sayands <sayandsarkar.1997@gmail.com>
2017-10-22 14:10:59 +0530

    Payment Handler Completed

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cookieSession = require("cookie-session");
 const passport = require("passport");
+const bodyParser = require("body-parser");
 const keys = require("./config/keys");
 require("./models/User");
 require("./services/passport");
@@ -10,6 +11,8 @@ mongoose.connect(keys.mongoURI);
 
 const app = express();
 //middlewares - small functions to modify incoming requests to the app for better response
+
+app.use(bodyParser.json());
 
 //use cookies to give the session time and keys
 app.use(
@@ -24,6 +27,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 require("./routes/authRoutes")(app);
+require("./routes/billingRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
