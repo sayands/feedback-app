b00c1ede06151de750c9522cf21baf7fd56d2aa0
sayands <sayandsarkar.1997@gmail.com>
2017-10-26 20:06:55 +0530

    Setup Mailer Constructors  and Boilerplate for Sending Emails

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,6 +1,9 @@
 const mongoose = require("mongoose");
 const requireLogin = require("../middlewares/requireLogin");
 const requireCredits = require("../middlewares/requireCredits");
+const Mailer = require("../services/Mailer");
+const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
+
 const Survey = mongoose.model("surveys");
 
 module.exports = app => {
@@ -17,5 +20,8 @@ module.exports = app => {
       _user: req.user.id,
       dateSent: Date.now()
     });
+
+    //Great place to send an email !!!
+    const Mailer = new Mailer(survey, surveyTemplate(survey));
   });
 };
--- services/Mailer.js
+++ services/Mailer.js
@@ -2,4 +2,40 @@ const sendgrid = require("sendgrid");
 const helper = sendgrid.mail;
 const keys = require("../config/keys");
 
-class Mailer extends Helper.Mail {}
+class Mailer extends helper.Mail {
+      constructor({ subject, recipients}, content){
+            super();
+
+            this.from_email = new helper.Email('no-reply@emaily.com');
+            this.subject = subject;
+            this.body = new helper.Content('text/html', content);
+            this.recipients = this.formatAddresses(recipients);
+
+            this.addContent(this.body);
+            this.addClickTracking();
+            this.addRecipients();
+      }
+formatAddresses(recipients) {
+    return recipients.map(({ email }) => {
+        return new helper.Email(email);
+    });
+}
+
+addClickTracking(){
+    const trackingSettings = new helper.trackingSettings();
+    const clickTracking = new helper.ClickTracking(true, true);
+
+    trackingSettings.setClickTracking(clickTracking);
+    this.addTrackingSettings(trackingSettings);
+}
+
+addRecipients(){
+    const personalize = new helper.Personalization();
+    this.recipients.forEach(recipient => {
+       personalize.addTo(recipient);
+    });
+    this.addPersonalization(personalize);
+ }
+} 
+
+module.exports = Mailer;
