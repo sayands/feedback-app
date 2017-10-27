2bd3c0b1959e7707f16f2e45c6a4f385c7ee6b2e
sayands <sayandsarkar.1997@gmail.com>
2017-10-27 22:49:44 +0530

    Sendgrid Mailer Completed and Tested

--- services/emailTemplates/surveyTemplate.js
+++ services/emailTemplates/surveyTemplate.js
@@ -1,3 +1,21 @@
-module.exports = (survey) => {
-    return '<div>' + survey.body + '</div>';
+const keys = require("../../config/keys");
+
+module.exports = survey => {
+  return `
+      <html>
+        <body>
+          <div style="text-align: center;">
+              <h3>T'd like your inputs</h3>
+              <p>Please answer the following questions:</p>
+              <p>${survey.body}</p>
+              <div>
+                <a href="${keys.redirectDomain}/api/surveys/thanks">Yes</a>
+              </div>
+              <div>
+                <a href="${keys.redirectDomain}/api/surveys/thanks">No</a>
+              </div>
+          </div>
+        </body>
+      </html>
+    `;
 };
